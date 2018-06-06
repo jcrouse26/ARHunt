0ba1241f3dfb6b1f3a1d15dad965762695d4078a
Jason Crouse <jcrouse26@gmail.com>
2018-06-06 14:23:42 -0700

    update multiView with pin graying and notifications

--- AR_Hunt/MapViewController.swift
+++ AR_Hunt/MapViewController.swift
@@ -1,18 +1,11 @@
-/*
+// Jason Crouse copyright (c) 2018
 //
+///
 ////
+/////
 ////////
 ////////////
 ////////////////
-//////// Copyright
-//// Jason Crouse
-/ 2 /\ 0 /\ 1 /\ 8 /
-/ 0 /
-/ 1 /
-/ 8 /
-*/
-
-
 
 import UIKit
 import MapKit
@@ -80,72 +73,90 @@ class MapViewController: UIViewController {
 }
 
 extension MapViewController: MKMapViewDelegate {
-	
-	func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
-		self.userLocation = userLocation.location
-	}
-	
-	func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
-		
-		// Return if user has selected "My Location" instead of a pin
-		if view.annotation?.title! == "My Location" { return }
-		
-		// Here you get the coordinate of the selected annotation.
-		let coordinate = view.annotation!.coordinate
-		if let userCoordinate = userLocation {
-			// Make sure the tapped item is within range of the users location.
-			if userCoordinate.distance(from: CLLocation(latitude: coordinate.latitude, longitude: coordinate.longitude)) <= 40 {
-				// Add to array of winnings
-				if let title = view.annotation!.title! {
-					winnings.append(title)
-					
-					// Display alert
-					let alert = UIAlertController(title: "Congrats!", message: "You're RICH! You've won \(title)", preferredStyle: UIAlertControllerStyle.alert)
-					
-					alert.addAction(UIAlertAction(title: "Thanks!", style: UIAlertActionStyle.default, handler: { (alert: UIAlertAction!) in
-					print(alert)
-					}))
-					self.present(alert, animated: true)
-					self.mapView.removeAnnotation(view.annotation!)
-					
-					/*
-					// create next object
-					let currentLat = coordinate.latitude
-					let currentLong = coordinate.longitude
-					let multiplier = 0.00135
-					let randDegrees = Double(arc4random_uniform(180)) - 90
-					let nextCoordinateLat = currentLat + multiplier*__cospi((randDegrees + previousDegrees)/180)
-					let nextCoordinateLong = currentLong + multiplier*__sinpi((randDegrees + previousDegrees)/180)
-					let newTarget = ARItem(itemDescription: "new", location: CLLocation(latitude: nextCoordinateLat, longitude: nextCoordinateLong), itemNode: nil)
-					let newAnnotation = MapAnnotation(location: newTarget.location.coordinate, item: newTarget)
-					self.mapView.addAnnotation(newAnnotation)
-					self.mapView.removeAnnotation(view.annotation!)
-					
-					previousDegrees = randDegrees + previousDegrees
-					// transitionToGameScreen()
-					*/
-				}
-			} else if userCoordinate.distance(from: CLLocation(latitude: coordinate.latitude, longitude: coordinate.longitude)) > 40 {
-				let distance = Int(userCoordinate.distance(from: CLLocation(latitude: coordinate.latitude, longitude: coordinate.longitude)))
-				let alert = UIAlertController(title: "Sorry", message: "You are \(distance) meters away. That's too far to get rich. Don't be lazy!", preferredStyle: UIAlertControllerStyle.alert)
-				
-				alert.addAction(UIAlertAction(title: "Okay", style: UIAlertActionStyle.default, handler: { (alert: UIAlertAction!) in
-					print(alert)
-				}))
-				self.present(alert, animated: true)
-			}
-		}
-	}
-	/*
-	// Transitions to AR, QUIZ, OR GAMESCREEN
-	func transitionToGameScreen() {
-	let storyboard = UIStoryboard(name: "Main", bundle: nil)
-	if let viewController = storyboard.instantiateViewController(withIdentifier: "") as? ViewController {
-	if let mapAnnotation = view.annotation as? MapAnnotation {
-	self.present(viewController, animated: true, completion: nil)
-	}
-	}
-	}
-	
-	*/
+    
+    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+        self.userLocation = userLocation.location
+    }
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        guard let annotation = annotation as? MapAnnotation else { return nil }
+        if annotation.captured == false { return nil }
+        let identifier = "pin"
+        var view: MKPinAnnotationView
+        if let dequeuedView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKPinAnnotationView {
+            dequeuedView.annotation = annotation
+            view = dequeuedView
+        } else {
+            view = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            view.pinTintColor = UIColor.darkGray
+        }
+        return view
+    }
+    
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        
+        // Return and deselect if user has selected "My Location" instead of a pin
+        if view.annotation?.title! == "My Location" {
+            self.mapView.deselectAnnotation(view.annotation, animated: true)
+            return
+        }
+        if view.reuseIdentifier! == "pin" {
+            // create an alert saying you've already won
+            let alert = UIAlertController(title: "Nice Try!", message: "You already collected this one. Get off your ass and collect a new one.", preferredStyle: UIAlertControllerStyle.alert)
+            
+            alert.addAction(UIAlertAction(title: "I'm sorry, won't happen again", style: UIAlertActionStyle.default, handler: { (alert: UIAlertAction!) in
+                //print(alert)
+            }))
+            self.present(alert, animated: true)
+            
+            // deselect this pin and return
+            self.mapView.deselectAnnotation(view.annotation, animated: true)
+            return
+        }
+        
+        
+        // Here you get the coordinate of the selected annotation.
+        let coordinate = view.annotation!.coordinate
+        
+        // Make sure the optional userLocation is populated.
+        if let userCoordinate = userLocation {
+            
+            // Make sure the tapped item is within range of the users location.
+            if userCoordinate.distance(from: CLLocation(latitude: coordinate.latitude, longitude: coordinate.longitude)) <= 400 {
+                
+                // Add to array of winnings ... Aka GET RICH!!!
+                
+                if let title = view.annotation!.title! {
+                    winnings.append(title)
+                    
+                    let alert = UIAlertController(title: "Congrats!", message: "You're RICH! You've won \(title)", preferredStyle: UIAlertControllerStyle.alert)
+                    
+                    alert.addAction(UIAlertAction(title: "Thanks!", style: UIAlertActionStyle.default, handler: { (alert: UIAlertAction!) in
+                        print(alert)
+                    }))
+                    self.present(alert, animated: true)
+                    
+                    // Add vibration so John's ladies can truly enjoy BitcoinGO
+                    AudioServicesPlayAlertSound(kSystemSoundID_Vibrate)
+                    
+                    // Attempt to create it as a MapAnnotation (custom class)
+                    guard let annotation = view.annotation as? MapAnnotation else { return }
+                    annotation.captured = true
+                    
+                    // Remove and add new annotation to map
+                    self.mapView.removeAnnotation(view.annotation!)
+                    self.mapView.addAnnotation(annotation)
+                }
+                
+            } else if userCoordinate.distance(from: CLLocation(latitude: coordinate.latitude, longitude: coordinate.longitude)) > 40 {
+                let distance = Int(userCoordinate.distance(from: CLLocation(latitude: coordinate.latitude, longitude: coordinate.longitude)))
+                let alert = UIAlertController(title: "Sorry", message: "You are \(distance) meters away. That's too far to get rich. Don't be lazy!", preferredStyle: UIAlertControllerStyle.alert)
+                
+                alert.addAction(UIAlertAction(title: "Okay", style: UIAlertActionStyle.default, handler: { (alert: UIAlertAction!) in }))
+                self.present(alert, animated: true)
+                self.mapView.deselectAnnotation(view.annotation, animated: true)
+                
+            }
+        }
+    }
+    
 }
